d3a0a7aafa64e21c7420ebee1d87577538ed3755
sadi394 <72342815+sadi394@users.noreply.github.com>
2024-03-30 01:41:48 +0500

    Add files via upload

---
+++ datatypes.js
@@ -0,0 +1,62 @@
+// Data Types
+// Strings
+// Example 1
+var brandname = "lakhany Silk Mills";
+console.log(brandname);
+console.log();
+// Example 2
+var qoute = "I am not afraid of an army of lions led by a sheep, I am afraid of an army of sheeps led by a lion ";
+var leader = "Alexander The Great";
+console.log("Once " + leader + "said " + qoute);
+console.log();
+// Numbers
+// Example 1
+var a = 7;
+var b = 2;
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a === b);
+// 2 types of code for written exponential equation --- This means 7^2  
+console.log(Math.pow(a, b));
+console.log(Math.pow(a, b));
+console.log();
+// Example 2 floating Numbers
+var num1 = 7.5;
+var num2 = 3.5;
+console.log(num1 % a);
+console.log(num2 % b);
+console.log(num1 * num2);
+console.log(num1 != num2);
+console.log();
+// Boolean Expresions
+var right = true;
+var wrong = false;
+console.log(right);
+console.log(wrong);
+console.log("Either " + right + " or " + wrong);
+var wrong = true;
+console.log("Either " + right + " or " + wrong);
+console.log();
+// Null Expressions
+// Null and Undefined both are diffrent properties but there pupose is comparatively same.
+var x = null;
+console.log(x);
+var y = undefined;
+console.log(y);
+console.log();
+var half;
+var full;
+console.log(half, full);
+console.log();
+// Symbols
+//var mysym = Symbol();
+//console.log(mysym);
+// var mysym = Symbol(1234);
+// console.log(mysym);
+// Big Numbers Representation (With diffrence)
+var num_without_n = 123455667788842387; // without n output will give rounded value
+console.log(num_without_n);
+var num_with_n = 123455667788858442588n; // with n output will be exact
+console.log(num_with_n);
